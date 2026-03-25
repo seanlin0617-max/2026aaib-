4e3e9fb942d6e09810338039a2ed232f0015e037
seanlin0617-max <seanlin0617@gmail.com>
2026-03-25 11:56:21 +0800

    Add files via upload

---
+++ week05/week05-2b.cpp
@@ -0,0 +1,14 @@
+
+//week05-2b.cpp SOIT_BASE_009
+#include <stdio.h>
+int main()
+{
+  int a[3];
+  int big= -9999999, small = 9999999;
+  for(int i=0;i<3;i++){
+      scanf("%d", &a[i]);
+      if(a[i]>big)big = a[i];
+      if(a[i]<small)small = a[i];
+    }
+   printf("%d\n", big-small);
+}
